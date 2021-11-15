650a3cd7d96caa16c001bf3571628ffdc4465bce
andersonNB <andersonaugustonb@ufps.edu.co>
2021-11-14 19:04:27 -0500

    PrimerCommit

---
+++ Views/Layouts/footer.php
@@ -0,0 +1,31 @@
+</div>
+<!-- /.content-wrapper -->
+
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 3.1.0
+    </div>
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="Util/js/jquery-3.6.0.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="Util/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="Util/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="Util/js/demo.js"></script>
+
+<!-- <script src="Util/js/bootstrap.min.js"></script> -->
+</body>
+
+</html>
